dd74faca26bd5b330eeeabbad927b4e036d0a616
Caio Leme <caioacleme@gmail.com>
2023-01-11 18:31:01 -0300

    Updated notes for day 7: CSS global
    Updated notes for day 7: CSS global

--- Projeto01/01-fundamentos-reactjs/src/App.jsx
+++ Projeto01/01-fundamentos-reactjs/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Header } from '../components/Header'
 import {Post} from './Post'
-import './style.css'
+import './global.css'
 
 export function App() {
   const [count, setCount] = useState(0)
