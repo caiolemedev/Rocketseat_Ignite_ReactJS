b1e7df3f983f0bb1cde5cd4c0d4a8fb01f508017
Caio Leme <caioacleme@gmail.com>
2023-01-13 10:47:12 -0300

    Updated day 9 - Component: Sidebar
    Updated day 9 - Component: Sidebar

--- Projeto01/01-fundamentos-reactjs/src/App.jsx
+++ Projeto01/01-fundamentos-reactjs/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { Header } from './components/Header'
 import {Post} from './Post'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -9,18 +11,22 @@ export function App() {
   return (
     <div>
       <Header/>
-      <Post 
-        author="Caio Leme" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia iure reprehenderit eaque quidem excepturi suscipit natus, amet officiis, aut nemo aspernatur labore ullam illo unde quo dolorum consequatur qui rem."
-      />
-      <Post
-        author="José Silva"
-        content="Um post muito legal!!" 
-      />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post 
+            author="Caio Leme" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia iure reprehenderit eaque quidem excepturi suscipit natus, amet officiis, aut nemo aspernatur labore ullam illo unde quo dolorum consequatur qui rem."
+          />
+          <Post
+            author="José Silva"
+            content="Um post muito legal!!" 
+          />
+        </main>
+      </div>
     </div>
     )
 }
---
+++ Projeto01/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,24 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return(
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt="Banner do perfil do usuário"
+      />
+
+      <div className={styles.profile}>
+        <strong>User Name</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
