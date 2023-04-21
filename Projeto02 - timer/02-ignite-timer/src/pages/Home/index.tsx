8e22b9a2b2457d48faa1fbdac375b7cf1bca1167
Caio Leme <caioacleme@gmail.com>
2023-04-21 19:22:29 -0300

    Updated: project2-lesson18
    Updated: project2-lesson18
    Form validation with zod

--- Projeto02 - timer/02-ignite-timer/src/pages/Home/index.tsx
+++ Projeto02 - timer/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
 
 import {
   CountdownContainer,
@@ -12,8 +13,18 @@ import {
   MinutesAmountInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa!'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser no máximo 60 minutos!'),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
